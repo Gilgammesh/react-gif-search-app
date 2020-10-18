7edb3f021c89d6e7c48a8ab3f7817723ac9e2f5f
Carlos Santander <carlos.santander@gmail.com>
2020-10-18 04:46:04 -0500

    build app a docs

---
+++ docs/precache-manifest.2810995cd484b9d5cdf542a3639cfcbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a890d2f4992885e2ff15c0eb10df1c9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9226f638e9d1377b535b",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "2d59a112d3490b4b5485",
+    "url": "/static/css/main.391b2fce.chunk.css"
+  },
+  {
+    "revision": "9226f638e9d1377b535b",
+    "url": "/static/js/2.26ec785f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.26ec785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d59a112d3490b4b5485",
+    "url": "/static/js/main.e3dba2ac.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
